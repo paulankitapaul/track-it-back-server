5863bf58f2fcf87715bffe8a484899372a757760
paulankitapaul <paulankita.ctg@gmail.com>
2024-12-31 18:54:28 +0600

    now can find by id

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/all-item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await itemCollection.findOne(query);
+            res.send(result);
+        });
+
         app.post('/all-item', async (req, res) => {
             const newItem = req.body;
             const result = await itemCollection.insertOne(newItem);
