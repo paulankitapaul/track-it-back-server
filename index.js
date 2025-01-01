df942da050d8251cbcf59cbc6fb84288e406d4c1
paulankitapaul <paulankita.ctg@gmail.com>
2025-01-01 17:15:15 +0600

    now can be delete from DB

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/all-item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         // store recovered items
         app.get('/recovered-item', async (req, res) => {
